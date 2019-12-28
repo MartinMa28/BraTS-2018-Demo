ee8e7eae1e829f60627d26a60cefa541b592ff62
Martin <1135485226@qq.com>
2019-12-27 19:49:06 -0800

    finish file upload

--- brats_3D_viewer/views.py
+++ brats_3D_viewer/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .forms import MRIScanForm
 from django.views import View
+from .models import MRIScan
 
 # Create your views here.
 # def index(request):
@@ -10,6 +11,18 @@ from django.views import View
 class HomeView(View):
     def get(self, request):
         form  = MRIScanForm()
-        context = {'form': form}
+        scans = MRIScan.objects.all()
+        context = {'form': form,
+                    'scans': scans}
 
         return render(request, 'brats_3D_viewer/index.html', context=context)
+
+    def post(self, request):
+        form = MRIScanForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+
+            return JsonResponse({'status': 'Success'})
+        else:
+            return JsonResponse({'status': 'Failed to upload MRI scans.'})
+
