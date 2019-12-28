2fb564dbaf448f5a55439d5e29f56db6555f8e53
Martin <1135485226@qq.com>
2019-12-27 17:39:48 -0800

    after first migration

--- brats_3D_viewer/models.py
+++ brats_3D_viewer/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class MRIScan(models.Model):
+    case_id = models.CharField(max_length=50, unique=True)
+    
+    # files will be uploaded to MEDIA_ROOT/scans
+    t1 = models.FileField(upload_to='scans')
+    t1ce = models.FileField(upload_to='scans')
+    t2 = models.FileField(upload_to='scans')
+    flair = models.FileField(upload_to='scans')
+
+    def __str__(self):
+        return self.case_id
+
+    def delete(self, *args, **kwargs):
+        # delete the uploaded files before calling
+        # the base class's delete method
+        self.t1.delete()
+        self.t1ce.delete()
+        self.t2.delete()
+        self.flair.delete()
+
+        super().delete(*args, **kwargs)
+
--- brats_async_demo/settings.py
+++ brats_async_demo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'brats_3D_viewer.apps.Brats3DViewerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
