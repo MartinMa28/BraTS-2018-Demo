bc9f23dcaf27bf657cb969695a13fdc7225b5bd9
Martin <1135485226@qq.com>
2019-12-29 12:09:14 -0800

    working submit

--- brats_3D_viewer/static/brats_3D_viewer/scripts.js
+++ brats_3D_viewer/static/brats_3D_viewer/scripts.js
@@ -1,17 +1,46 @@
+function updateScans(data) {
+    let scanList = document.querySelector('div#scan-list ul');
+    let oldListItems = document.querySelectorAll('div#scan-list ul li');
+    oldListItems.forEach(item => {
+        scanList.removeChild(item);
+    })
+    
+    
+    let newScans = data.scans;
+
+    newScans.forEach(scan => {
+        let newItem = document.createElement('li');
+        newItem.innerHTML = `
+        <li>${scan[0]}
+            <button class="infer-btn" data-case-id="${scan[0]}" data-id="${scan[1]}">Infer</button>
+            <button class="view-btn" data-case-id="${scan[0]}" data-id="${scan[1]}">View 3D</button>
+            <button class="delete-btn" data-case-id="${scan[0]}" data-id="${scan[1]}">Delete</button>
+        </li>
+        `;
+    })
+
+}
+
+
+
 let delBtns = document.querySelectorAll('.delete-btn');
 delBtns.forEach(btn => btn.addEventListener('click', () => {
     let scanId = btn.dataset.id;
-    let deleteURL = RESTfulAPIS['delete'];
+    let deleteURL = RESTfulAPIS.delete;
+    let scansURL = RESTfulAPIS.get_scans;
 
     axios.delete(`http://${HOSTNAME}/${deleteURL.split('/')[1]}/${scanId}`)
-        .then((response) => {
+        .then(response => {
             console.log(response.data);
             console.log(response.status);
             console.log(response.statusText);
-            console.log(response.headers);
-            console.log(response.config);
+            
+            axios.get(`http://${HOSTNAME}${scansURL}`)
+                .then(response => {
+                    updateScans(response);
+                })
         })
-        .catch((error) => {
+        .catch(error => {
             console.log(error);
         });
 }));
