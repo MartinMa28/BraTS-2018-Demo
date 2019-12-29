4f63c745d771319f7155c5052d3c0256a2060876
Martin <1135485226@qq.com>
2019-12-29 11:39:39 -0800

    finish delete

--- brats_3D_viewer/static/brats_3D_viewer/scripts.js
+++ brats_3D_viewer/static/brats_3D_viewer/scripts.js
@@ -1,4 +1,17 @@
 let delBtns = document.querySelectorAll('.delete-btn');
 delBtns.forEach(btn => btn.addEventListener('click', () => {
-    console.log(`ready to delete ${btn.dataset.id} ${btn.dataset.caseId}`);
+    let scanId = btn.dataset.id;
+    let deleteURL = RESTfulAPIS['delete'];
+
+    axios.delete(`http://${HOSTNAME}/${deleteURL.split('/')[1]}/${scanId}`)
+        .then((response) => {
+            console.log(response.data);
+            console.log(response.status);
+            console.log(response.statusText);
+            console.log(response.headers);
+            console.log(response.config);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 }));
--- brats_3D_viewer/urls.py
+++ brats_3D_viewer/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from .views import HomeView, delete_scan, get_scans
 
+app_name = 'brats_3D_viewer'
+
 urlpatterns = [
     path('', HomeView.as_view(), name='index'),
-    path('delete/<int:scan_id>', delete_scan),
-    path('scans', get_scans)
+    path('delete/<int:scan_id>', delete_scan, name='delete'),
+    path('scans', get_scans, name='get_scans')
 ]
